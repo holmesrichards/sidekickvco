9cb9e9799da42721d2224be709bff238b0b2932c
Rich Holmes <rs.holmes@gmail.com>
2022-07-08 11:15:07 -0400

    New b1 Gerbers

--- Hardware/PCB/gerber_gen.sh
+++ Hardware/PCB/gerber_gen.sh
@@ -1,10 +1,10 @@
 pushd sidekickvco
-kikit separate --source 'annotation; ref: B1' sidekickvco.kicad_pcb  'Separate PCBs/sidekickvco_b1.kicad_pcb'
+kikit separate --source 'rectangle; tlx: 90mm; tly: 30mm; brx: 190mm; bry: 200mm' 'sidekickvco.kicad_pcb' 'Separate PCBs/sidekickvco_b1.kicad_pcb'
 kikit fab jlcpcb --no-drc --nametemplate "sidekickvco_b1_{}" 'Separate PCBs/sidekickvco_b1.kicad_pcb' ..
 popd
-mv gerbers sidekickvco_b1_gerbers
-pushd sidekickvco
-kikit separate --source 'annotation; ref: B2' sidekickvco.kicad_pcb  'Separate PCBs/sidekickvco_b2.kicad_pcb'
-kikit fab jlcpcb --no-drc --nametemplate "sidekickvco_b2_{}" 'Separate PCBs/sidekickvco_b2.kicad_pcb' ..
-popd
-mv gerbers sidekickvco_b2_gerbers
+mv gerber sidekickvco_b1_gerbers
+#pushd sidekickvco
+#kikit separate --source 'annotation; ref: B2' sidekickvco.kicad_pcb  'Separate #PCBs/sidekickvco_b2.kicad_pcb'
+#kikit fab jlcpcb --no-drc --nametemplate "sidekickvco_b2_{}" 'Separate PCBs/sid#ekickvco_b2.kicad_pcb' ..
+#popd
+#mv gerber sidekickvco_b2_gerbers
